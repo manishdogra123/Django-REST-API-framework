5217e9843a71ce68a9f969480b40baeac2184f8c
manish <erdogra141@gmail.com>
2021-09-23 14:59:34 +0530

    create class base view and get all object

--- home/views.py
+++ home/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.views import ApiView
+from rest_framework.response import Response
+from .models import Course,Courseserializer
+
 
 # Create your views here.
+class Courselistview(ApiView):
+    def get(self):
+        course = Course.objects.all()
+        serializer = Courseserializer(course, many=True)
+        return Response(serializer.data)
+       
