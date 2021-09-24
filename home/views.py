e23c568868c84f103ab780025e2c324a0849a08c
manish <erdogra141@gmail.com>
2021-09-24 17:03:38 +0530

    put request for update & modify particuler instanse a table data from database

--- home/views.py
+++ home/views.py
@@ -1,13 +1,13 @@
 from django.http.request import RAISE_ERROR
 from django.http.response import JsonResponse
 from django.shortcuts import render
-from rest_framework import response
 from rest_framework.serializers import Serializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Course
 from django.http import Http404
 from .serializer import Courseserializer
+from rest_framework import status
 
 
 # Create your views here.
@@ -21,7 +21,7 @@ class Courselistview(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        return Response(serializer.errors) 
+        return Response(serializer.errors)
 
 class Courselist_detail(APIView):
     def get_object(self,pk):
@@ -34,3 +34,14 @@ class Courselist_detail(APIView):
         course_tutorial = self.get_object(pk)
         serializer = Courseserializer(course_tutorial)
         return Response(serializer.data)
+
+    def delete(self,request,pk,formet=None):
+        Serializer = self.get_object(pk)
+        Serializer.delete() 
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    def put(self,request,pk,formet=None):
+        User_deatil = self.get_object(pk)
+        Serializer = Courseserializer(User_deatil,data=request.data)
+        if Serializer.is_valid():
+            Serializer.save()
+        return Response(status=status.HTTP_400_BAD_REQUEST)    
