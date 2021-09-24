a96a1d21f8f23eab43bacf5529cfa60288f421f3
manish <erdogra141@gmail.com>
2021-09-24 17:52:43 +0530

    just urls handle

---
+++ home/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path,include
+from .views import Courselistview,Courselist_detail
+from home.views import Courselistview,Courselist_detail
+
+urlpatterns = [
+
+        path('course', Courselistview.as_view()),
+        path('course/<int:pk>', Courselist_detail.as_view()),
+
+]      
